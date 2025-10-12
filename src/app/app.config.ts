1225dc0a8476109f8466aacfa7ec4cd4e33aeda4
Danbas21 <149043410+Danbas21@users.noreply.github.com>
2025-10-12 11:13:26 -0600

    Initial commit: Angular 20 + Firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from '@env/environment';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from '@core/interceptors/auth.interceptor';
+import { errorInterceptor } from '@core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor, errorInterceptor])
+    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'TU_API_KEY',
+    authDomain: 'tu-proyecto.firebaseapp.com',
+    projectId: 'tu-proyecto',
+    storageBucket: 'tu-proyecto.appspot.com',
+    messagingSenderId: '123456789',
+    appId: '1:123456789:web:abcdef123456'
+  },
+  stripe: {
+    publishableKey: 'pk_test_...'
+  },
+  api: {
+    baseUrl: 'http://localhost:5001/tu-proyecto/us-central1'
+  }
+};
